021659c67569e708ac5875b3d01fdff8dd841b3d
remind1990 <tigerstylest00@gmail.com>
2023-10-05 16:47:23 -0230

    api problem

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ function generateUniqueId() {
 //     });
 // });
 
-app.post('api/resize-images', upload.any(), async (req, res) => {
+app.post('/api/resize-images', upload.any(), async (req, res) => {
   try {
     if (!req.files || req.files.length === 0) {
       return res.status(400).send('No images uploaded.');
